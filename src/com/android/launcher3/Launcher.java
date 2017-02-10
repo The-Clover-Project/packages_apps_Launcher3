359edc6cc787b7a3e5928df5e1a3bbd5696115e5
Thecrazyskull <anaskarbila@gmail.com>
2017-02-10 09:38:50 -0500

    Launcher3: Double tap on home screen to turn off screen
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: I554c005d5e523aca0842c78a353686e86af1a7f2

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -2664,6 +2664,11 @@ public class Launcher extends StatefulActivity<LauncherState>
         // Overridden
     }
 
+    /** To be overrideden by subclasses */
+    public void onSleepEvent(MotionEvent ev) {
+        // Overridden
+    }
+
     public void closeOpenViews() {
         closeOpenViews(true);
     }
