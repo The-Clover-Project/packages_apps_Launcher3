632d2dc86829b0eae1ed15ab0db1653025c64559
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-07-31 23:43:51 +0530

    Launcher3: Add a toggle for smartspace
    [DennySPb]: adapt to 14-qpr2
    [HELLBOY017]: adapt to 15, 16
    
    Change-Id: I3b2681d51197642ba0957111db455937c0c376e8
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -445,6 +445,7 @@ public class Launcher extends StatefulActivity<LauncherState>
                         case Utilities.KEY_SHORT_PARALLAX:
                         case Utilities.KEY_SINGLE_PAGE_CENTER:
                         case Utilities.KEY_DRAWER_SEARCH:
+                        case Utilities.KEY_SMARTSPACE:
                             mNeedsRestart = true;
                             break;
                         default:
