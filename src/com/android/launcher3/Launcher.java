21829dfd0b7f92c985237ddb581688e378db1b4b
Pranav Vashi <neobuddy89@gmail.com>
2024-08-31 22:57:03 +0530

    Launcher3: Allow setting stroke width for hotseat search bar
    Ref: https://github.com/LawnchairLauncher/lawnchair/commit/44d54b53b22bd8dcdab8fe1deeaa1a868550b2a2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -447,6 +447,7 @@ public class Launcher extends StatefulActivity<LauncherState>
                         case Utilities.KEY_DRAWER_SEARCH:
                         case Utilities.KEY_SMARTSPACE:
                         case Utilities.KEY_HOTSEAT_QSB_OPACITY:
+                        case Utilities.KEY_HOTSEAT_QSB_STROKE_WIDTH:
                             mNeedsRestart = true;
                             break;
                         default:
