cc793c2d0b62e7003c2e97e1b112f6285126aea2
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-12 07:43:03 +0800

    Launcher3: Improve restart logic
    issue
    https://github.com/AxionAOSP/issue_tracker/issues/46
    
    Change-Id: Ie89c528621ffc44a7d6d0b4680d427c43823f77b
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1322,8 +1322,6 @@ public class Launcher extends StatefulActivity<LauncherState>
 
         DragView.removeAllViews(this);
         TraceHelper.INSTANCE.endSection();
-
-        LauncherAppState.INSTANCE.executeIfCreated(app -> app.checkIfRestartNeeded());
     }
 
     @Override
