a037db7878aa3ed58b9a59839541ca2e0c6e8cb2
Pranav Vashi <neobuddy89@gmail.com>
2026-02-15 06:08:47 +0530

    Launcher3: Fix row height customization in app drawer
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/913
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -862,7 +862,8 @@ public class DeviceProfile {
                     mResponsiveAllAppsWidthSpec,
                     mResponsiveAllAppsHeightSpec,
                     mIconSizeSteps,
-                    isVerticalBarLayout()
+                    isVerticalBarLayout(),
+                    allAppsCellHeightMultiplier
             );
             updateAllAppsWithResponsiveMeasures();
         } else {
@@ -874,7 +875,8 @@ public class DeviceProfile {
                     mTypeIndex,
                     scale,
                     getWorkspaceIconProfile().getIconSizePx(),
-                    mWorkspaceProfile.getIconDrawablePaddingOriginalPx()
+                    mWorkspaceProfile.getIconDrawablePaddingOriginalPx(),
+                    allAppsCellHeightMultiplier
             );
         }
         updateAllAppsContainerWidth();
